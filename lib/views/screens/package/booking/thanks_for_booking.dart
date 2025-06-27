695568ac10a2a0930f681a6cad3394e4c6d9f3e2
raj <raj.appdid@gmail.com>
2025-06-27 18:09:18 +0530

    bookings.dart , thanksforbooking

--- lib/views/screens/package/booking/thanks_for_booking.dart
+++ lib/views/screens/package/booking/thanks_for_booking.dart
@@ -56,86 +56,84 @@ class ThanksForBookingPage extends StatelessWidget {
                           ),
                         ],
                       ),
-                      child: SafeArea(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Center(
-                              child: Text(
-                                'Thanks for Booking',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xFF4A90E2),
-                                ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Center(
+                            child: Text(
+                              'Thanks for Booking',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF4A90E2),
                               ),
                             ),
-                            Center(
-                              child: Column(
-                                children: [
-                                  Image.asset(
-                                    'assets/images/Thumb.gif',
-                                    height: 200,
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const Text(
-                                        'Do you want to cancel your Shoot?',
+                          ),
+                          Center(
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  'assets/images/Thumb.gif',
+                                  height: 200,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      'Do you want to cancel your Shoot?',
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 14,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        Get.snackbar(
+                                          'Cancel Booking',
+                                          'Cancellation process initiated...',
+                                          backgroundColor: Colors.red,
+                                          colorText: Colors.white,
+                                        );
+                                      },
+                                      child: Text(
+                                        ' Cancel',
                                         style: TextStyle(
-                                          color: Colors.grey,
+                                          color: Colors.red.shade600,
                                           fontSize: 14,
                                         ),
                                         textAlign: TextAlign.center,
                                       ),
-                                      GestureDetector(
-                                        onTap: () {
-                                          Get.snackbar(
-                                            'Cancel Booking',
-                                            'Cancellation process initiated...',
-                                            backgroundColor: Colors.red,
-                                            colorText: Colors.white,
-                                          );
-                                        },
-                                        child: Text(
-                                          ' Cancel',
-                                          style: TextStyle(
-                                            color: Colors.red.shade600,
-                                            fontSize: 14,
-                                          ),
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                            const SizedBox(height: 20),
-                            const Text(
-                              'Shoot Details',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
+                          ),
+                          const SizedBox(height: 20),
+                          const Text(
+                            'Shoot Details',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
                             ),
-                            const SizedBox(height: 10),
-                            _buildShootDetailRow("Cuteness", 'Rs. 5999'),
-                            _buildShootDetailRow("Moments", 'Rs. 1599'),
-                            _buildShootDetailRow('Addon Price', 'Rs. 0'),
-                            _buildShootDetailRow('Promo Discount', 'Rs. 0'),
-                            const Divider(),
-                            Obx(
-                              () => _buildShootDetailRow(
-                                'Total Payment',
-                                'Rs. ${controller.totalPayment.value.toStringAsFixed(0)}',
-                                isBold: true,
-                              ),
+                          ),
+                          const SizedBox(height: 10),
+                          _buildShootDetailRow("Cuteness", 'Rs. 5999'),
+                          _buildShootDetailRow("Moments", 'Rs. 1599'),
+                          _buildShootDetailRow('Addon Price', 'Rs. 0'),
+                          _buildShootDetailRow('Promo Discount', 'Rs. 0'),
+                          const Divider(),
+                          Obx(
+                            () => _buildShootDetailRow(
+                              'Total Payment',
+                              'Rs. ${controller.totalPayment.value.toStringAsFixed(0)}',
+                              isBold: true,
                             ),
+                          ),
 
-                            // Dynamically build PackageCards for selected packages
-                          ],
-                        ),
+                          // Dynamically build PackageCards for selected packages
+                        ],
                       ),
                     ),
                     Obx(
